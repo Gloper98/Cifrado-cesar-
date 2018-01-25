5d93cc58bf0adcdf94ef8b8331dc5bfb0bf139fb
Gloper98 <littleannygutlop_2014@outlook.es>
2018-01-25 17:46:57 -0500

    Updating js vanilla version

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 function begin(){
-  var $msge = $('#encode');
-	var $encodeBtn = $('#encode-btn');
-	var $decodeBtn = $('#decode-btn');
-	var $code = $('#code');
-	var $valueInput = $('#value-input');
-	function encode(e){
-		$valueInput.text($msge.val());
+  var msge = document.getElementById('encode');
+	var encodeBtn = document.getElementById('encode-btn');
+	var decodeBtn = document.getElementById('decode-btn');
+	var code = document.getElementById('code');
+	var valueInput = document.getElementById('value-input');
+	var encode =(e) =>{
+		valueInput.innerText = msge.value;
 		var newString = [];
-		var $inputValue = $msge.val();
-		if($inputValue.length === 0){
-			$code.text('You should enter at least a letter in the input');
+		var inputValue = msge.value;
+		if(inputValue.length === 0){
+			code.innerText='You should enter at least a letter in the input';
 		} else {
-			var firstStep = $inputValue.replace(/(^\s*)|(\s*$)/gi,'').replace(/[ ]{2,}/gi,' ').toLowerCase();
-			console.log(firstStep);
+			var firstStep = inputValue.replace(/(^\s*)|(\s*$)/gi,'').replace(/[ ]{2,}/gi,' ').toLowerCase();
 			for(var i =0;i< firstStep.length;i++){
 				if(firstStep[i]*0 !== 0){
 					if(firstStep.charCodeAt(i) >= 97 && firstStep.charCodeAt(i) <= 122){
@@ -20,20 +19,21 @@ function begin(){
 					  var newAscciCode = (ascciCode-65+33)%26+65;
 						var encodedString =String.fromCharCode(newAscciCode);
 						newString.push(encodedString);
+						console.log(ascciCode);
 				  }
 				} else if (firstStep.charCodeAt(i) === 32) {
 					newString.push(' ');
 				} else {
-				  $msge.val('');
+				  msge.value='';
 					newString = ['You can not enter numbers,',' just letters please :)'];
 					}
-				  $msge.val('');
-				  $code.text(newString.join(''));
+				  msge.value='';
+				  code.innerText = newString.join('');
 			}
 		}
-		$msge.val('');
+		msge.value='';
 	}
-	$encodeBtn.click(encode);
+	encodeBtn.addEventListener('click', encode);
 }
 
-$(document).ready(begin);
+window.onload= begin;
