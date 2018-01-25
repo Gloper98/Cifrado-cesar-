6ee6c39998403295cc13f659dde2d12ca1bab070
Gloper98 <littleannygutlop_2014@outlook.es>
2018-01-25 11:05:21 -0500

    Updating new files and correcting errors

---
+++ js/app.js
@@ -0,0 +1,69 @@
+function begin(){
+  var $msge = $('#encode');
+	var $encodeBtn = $('#encode-btn');
+	var $decodeBtn = $('#decode-btn');
+	var $code = $('#code');
+	
+	function reload(){
+		window.location.href='index.html';
+	}
+	
+	function encode(e){
+		var newString = [];
+		var $inputValue = $msge.val();
+		if($inputValue.length === 0){
+			alert('Debes ingesar una palabra');
+		} else {
+			var firstStep = $inputValue.replace(/(^\s*)|(\s*$)/gi,'').replace(/[ ]{2,}/gi,' ').toLowerCase();
+			console.log(firstStep);
+			for(var i =0;i< firstStep.length;i++){
+				if(firstStep[i]*0 !== 0){
+					if(firstStep.charCodeAt(i) >= 97 && firstStep.charCodeAt(i) <= 122){
+					  var ascciCode =firstStep.charCodeAt(i);
+					  var newAscciCode = (ascciCode-65+33)%26+65;
+						var encodedString =String.fromCharCode(newAscciCode);
+						newString.push(encodedString);
+				  }
+				} else if (firstStep.charCodeAt(i) === 32) {
+					newString.push(' ');
+				} else {
+				  $msge.val('');
+					$encodeBtn.text('Try again');
+					$encodeBtn.off('click').click(reload);
+					}
+			}
+		}
+		$code.text(newString.join(''));
+	}
+	
+	function decode(){
+		var newString = [];
+		var $inputValue = $msge.val();
+		if($inputValue.length === 0){
+			alert('Debes ingesar una palabra');
+		} else {
+			var firstStep = $inputValue.replace(/(^\s*)|(\s*$)/gi,'').replace(/[ ]{2,}/gi,' ').toLowerCase();
+			console.log(firstStep);
+			for(var i =0;i< firstStep.length;i++){
+				if(firstStep[i]*0 !== 0){
+					if(firstStep.charCodeAt(i) >= 97 && firstStep.charCodeAt(i) <= 122){
+					  var ascciCode =firstStep.charCodeAt(i);
+					  var newAscciCode = ascciCode-(33%26);
+						var encodedString =String.fromCharCode(newAscciCode);
+						newString.push(encodedString);
+				  }
+				} else if (firstStep.charCodeAt(i) === 32) {
+					newString.push(' ');
+				} else {
+				  $msge.val('');
+					$encodeBtn.text('Try again');
+					$encodeBtn.off('click').click(reload);
+					}
+			}
+		}
+		$code.text(newString.join(''));
+	}
+	$encodeBtn.click(encode);
+}
+
+$(document).ready(begin);
